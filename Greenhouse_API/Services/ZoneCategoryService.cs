c65279411afa47fb44887a32ed2c505355c681b0
AlexMjbl <a.belanger132@gmail.com>
2025-12-16 21:25:07 -0500

    Refactored ZonecategoryService

--- Greenhouse_API/Services/ZoneCategoryService.cs
+++ Greenhouse_API/Services/ZoneCategoryService.cs
@@ -8,37 +8,143 @@ namespace Greenhouse_API.Services
 {
     public class ZoneCategoryService : IZoneCategoryService
     {
-        private SerreContext _context;
+        private IRepository<ZoneCategory> _repository;
         private readonly ILogger<ZoneCategoryService> _logger;
-        public ZoneCategoryService(SerreContext context, ILogger<ZoneCategoryService> logger)
+        public ZoneCategoryService(IRepository<ZoneCategory> repository, ILogger<ZoneCategoryService> logger)
         {
-            _context = context;
+            _repository = repository;
             _logger = logger;
         }
 
-        public Task<IEnumerable<ZoneCategoryDto>> GetAllAsync()
+        public async Task<IEnumerable<ZoneCategoryDto>> GetAllAsync()
         {
-            throw new NotImplementedException();
+            _logger.LogInformation("Retrieving all zone categories");   
+            var categories = await _repository.GetAllAsync();
+            return categories.Select(c => new ZoneCategoryDto
+            {
+                Id = c.Id,
+                Name = c.Name,
+                HumidityMinPct = c.HumidityMinPct,
+                HumidityMaxPct = c.HumidityMaxPct,
+                LuminosityMinLux = c.LuminosityMinLux,
+                LuminosityMaxLux = c.LuminosityMaxLux,
+                TemperatureMinC = c.TemperatureMinC,
+                TemperatureMaxC = c.TemperatureMaxC,
+                PressureMinPa = c.PressureMinPa,
+                PressureMaxPa = c.PressureMaxPa,
+                CreatedAt = c.CreatedAt
+            });
         }
 
-        public Task<ZoneCategoryDto?> GetByIdAsync(int id)
+        public async Task<ZoneCategoryDto?> GetByIdAsync(int id)
         {
-            throw new NotImplementedException();
+            var categorie = await _repository.GetByIdAsync(id);
+            if (categorie == null)
+            {
+                _logger.LogWarning("Zone category with ID: {ZoneCategoryId} not found", id);
+                return null;
+            }
+
+            _logger.LogInformation("Zone category with ID: {ZoneCategoryId} retrieved", id);
+            return new ZoneCategoryDto
+            {
+                Id = categorie.Id,
+                Name = categorie.Name,
+                HumidityMinPct = categorie.HumidityMinPct,
+                HumidityMaxPct = categorie.HumidityMaxPct,
+                LuminosityMinLux = categorie.LuminosityMinLux,  
+                LuminosityMaxLux = categorie.LuminosityMaxLux,
+                TemperatureMinC = categorie.TemperatureMinC,
+                TemperatureMaxC = categorie.TemperatureMaxC,
+                PressureMinPa = categorie.PressureMinPa,
+                PressureMaxPa = categorie.PressureMaxPa,
+                CreatedAt = categorie.CreatedAt
+            };
         }
 
-        public Task<ZoneCategoryDto> CreateAsync(ZoneCategoryWriteDto dto)
+        public async Task<ZoneCategoryDto> CreateAsync(ZoneCategoryWriteDto dto)
         {
-            throw new NotImplementedException();
+            var category = new ZoneCategory
+            {
+                Name = dto.Name,
+                HumidityMinPct =  dto.HumidityMinPct,
+                HumidityMaxPct = dto.HumidityMaxPct,
+                LuminosityMinLux = dto.LuminosityMinLux,
+                LuminosityMaxLux = dto.LuminosityMaxLux,
+                TemperatureMinC = dto.TemperatureMinC,
+                TemperatureMaxC = dto.TemperatureMaxC,
+                PressureMinPa = dto.PressureMinPa,
+                PressureMaxPa = dto.PressureMaxPa,
+                CreatedAt = DateTime.UtcNow
+            };
+
+            await _repository.AddAsync(category);
+            _logger.LogInformation("Zone category created with ID: {ZoneCategoryId}", category.Id);
+
+            return new ZoneCategoryDto
+            {
+                Id = category.Id,
+                Name = category.Name,
+                HumidityMinPct = category.HumidityMinPct,
+                HumidityMaxPct = category.HumidityMaxPct,
+                LuminosityMinLux = category.LuminosityMinLux,
+                LuminosityMaxLux = category.LuminosityMaxLux,
+                TemperatureMinC = category.TemperatureMinC,
+                TemperatureMaxC = category.TemperatureMaxC,
+                PressureMinPa = category.PressureMinPa,
+                PressureMaxPa = category.PressureMaxPa,
+                CreatedAt = category.CreatedAt
+            };
         }
 
-        public Task<ZoneCategoryDto> UpdateAsync(int id, ZoneCategoryWriteDto dto)
+        public async Task<ZoneCategoryDto> UpdateAsync(int id, ZoneCategoryWriteDto dto)
         {
-            throw new NotImplementedException();
+            var categorie = await _repository.GetByIdAsync(id);
+            if (categorie == null)
+            {
+                _logger.LogWarning("Zone category with ID: {ZoneCategoryId} not found for update", id);
+                throw new KeyNotFoundException("Zone category not found");
+            }
+
+            categorie.Name = dto.Name;
+            categorie.HumidityMinPct = dto.HumidityMinPct;
+            categorie.HumidityMaxPct = dto.HumidityMaxPct;
+            categorie.LuminosityMinLux = dto.LuminosityMinLux;
+            categorie.LuminosityMaxLux = dto.LuminosityMaxLux;
+            categorie.TemperatureMinC = dto.TemperatureMinC;
+            categorie.TemperatureMaxC = dto.TemperatureMaxC;
+            categorie.PressureMinPa = dto.PressureMinPa;
+            categorie.PressureMaxPa = dto.PressureMaxPa;
+
+            await _repository.SaveAsync();
+            _logger.LogInformation("Zone category with ID: {ZoneCategoryId} updated", id);
+
+            return new ZoneCategoryDto
+            {
+                Id = categorie.Id,
+                Name = categorie.Name,
+                HumidityMinPct = categorie.HumidityMinPct,
+                HumidityMaxPct = categorie.HumidityMaxPct,
+                LuminosityMinLux = categorie.LuminosityMinLux,
+                LuminosityMaxLux = categorie.LuminosityMaxLux,
+                TemperatureMinC = categorie.TemperatureMinC,
+                TemperatureMaxC = categorie.TemperatureMaxC,
+                PressureMinPa = categorie.PressureMinPa,
+                PressureMaxPa = categorie.PressureMaxPa,
+                CreatedAt = categorie.CreatedAt
+            };
         }
 
-        public Task DeleteAsync(int id)
+        public async Task DeleteAsync(int id)
         {
-            throw new NotImplementedException();
+            var deleted = await _repository.DeleteAsync(id);
+            if (!deleted)
+            {
+                _logger.LogWarning("Zone category with ID {ZoneCategoryId} not found for deletion", id);
+                throw new KeyNotFoundException("Zone category not found");  
+            }
+
+            _logger.LogInformation("Zone category with ID {ZoneCategoryId} deleted", id);
         }
     }
 }
