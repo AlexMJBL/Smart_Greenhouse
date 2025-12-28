e7c48d5a131dd29c6ae3571a72d74cd8430a7fc0
abela <abela@ALEX>
2025-12-28 14:23:43 -0500

    Added base service proxy for greenhouse mvc

---
+++ Greenhouse_Ressource_MVC/Interfaces/IServiceProxy.cs
@@ -0,0 +1,11 @@
+﻿namespace Greenhouse_Ressource_MVC.Interfaces
+{
+    public interface IServiceProxy<T>
+    {
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<T?> GetByIdAsync(int id);
+        Task<T> CreateAsync(T entity);
+        Task<T> UpdateAsync(int id, T entity);
+        Task DeleteAsync(int id);
+    }
+}
---
+++ Greenhouse_Ressource_MVC/Services/ServiceProxy.cs
@@ -0,0 +1,66 @@
+﻿using Greenhouse_Ressource_MVC.Interfaces;
+
+namespace Greenhouse_Ressource_MVC.Services
+{
+    public class ServiceProxy<T> : IServiceProxy<T> where T : class
+    {
+        protected readonly IHttpClientFactory _httpClientFactory;
+        protected readonly string _baseUrl;
+
+        protected ServiceProxy(IHttpClientFactory httpClientFactory, string baseUrl)
+        {
+            _httpClientFactory = httpClientFactory;
+            _baseUrl = baseUrl;
+        }
+
+        protected HttpClient CreateClient()
+        {
+            return _httpClientFactory.CreateClient("GreenhouseAPI");
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+            var client = CreateClient();
+            return await client.GetFromJsonAsync<IEnumerable<T>>(_baseUrl) ?? Enumerable.Empty<T>();
+        }
+
+        public async Task<T?> GetByIdAsync(int id)
+        {
+            var client = CreateClient();
+            return await client.GetFromJsonAsync<T>($"{_baseUrl}/{id}");
+        }
+
+        public async Task<T> CreateAsync(T entity)
+        {
+            var client = CreateClient();
+            var response = await client.PostAsJsonAsync(_baseUrl, entity);
+
+            response.EnsureSuccessStatusCode();
+
+            return await response.Content.ReadFromJsonAsync<T>() ?? throw new Exception("Error while creating the entity");
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            var client = CreateClient();
+            var response = await client.DeleteAsync($"{_baseUrl}/{id}");
+
+            response.EnsureSuccessStatusCode();
+        }
+
+        public async Task<T> UpdateAsync(int id, T entity)
+        {
+            var client = CreateClient();
+            var response = await client.PutAsJsonAsync($"{_baseUrl}/{id}", entity);
+
+            response.EnsureSuccessStatusCode();
+
+
+            if (response.Content.Headers.ContentLength == 0)
+                throw new Exception("API returned no content after update");
+
+            return await response.Content.ReadFromJsonAsync<T>() ?? throw new Exception("Error while updating the entity");
+        }
+    }
+}
+ 
