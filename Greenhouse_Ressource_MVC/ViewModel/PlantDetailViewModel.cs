16f8bdd31076114af90c14ee6abd11aa0c5dfc9b
AlexMjbl <a.belanger132@gmail.com>
2026-01-02 16:44:25 -0500

    Added viewModel for detail view

---
+++ Greenhouse_Ressource_MVC/ViewModel/PlantDetailViewModel.cs
@@ -0,0 +1,12 @@
+
+using Greenhouse_Ressource_MVC.Dtos;
+
+namespace Greenhouse_Ressource_MVC.ViewModel
+{
+    public class PlantDetailViewModel
+    {
+        public PlantDto Plant { get; set; }
+        public SpecimenDto Specimen{ get; set; }
+        public ZoneDto Zone {get;set;}
+    }
+}
