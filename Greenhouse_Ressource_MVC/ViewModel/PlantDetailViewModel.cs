ea8efe5ade5cd72a7a1de30ce694d0c9a3e4c58c
abela <abela@ALEX>
2026-01-02 21:04:06 -0500

    Refactored ressource mvc detail views to use view models

--- Greenhouse_Ressource_MVC/ViewModel/PlantDetailViewModel.cs
+++ Greenhouse_Ressource_MVC/ViewModel/PlantDetailViewModel.cs
@@ -8,5 +8,6 @@ namespace Greenhouse_Ressource_MVC.ViewModel
         public PlantDto Plant { get; set; }
         public SpecimenDto Specimen{ get; set; }
         public ZoneDto Zone {get;set;}
+        public PlantDto? momPlant { get; set; }
     }
 }
